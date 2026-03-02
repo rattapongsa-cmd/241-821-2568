59e5bad5f841d08b2fe65e22af54dbfe83db2bd8
rattapong-cmd <rattapong.sa@ku.th>
2026-03-02 17:14:16 +0700

    Commit

--- fontend/index.js
+++ fontend/index.js
@@ -1,32 +1,41 @@
-function submitData() {
-    // ดึง Element ต่างๆ มาเก็บไว้ในตัวแปร
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
+const submitData = async () => {
+    let firstNameDOM = document.querySelector('input[name="firstname"]');
+    let lastNameDOM = document.querySelector('input[name="lastname"]');
+    let ageDOM = document.querySelector('input[name="age"]');
+    let genderDOM = document.querySelector('input[name="gender"]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
+    let descriptionDOM = document.querySelector('textarea[name="description"]');
 
-    // ส่วนของ Logic การวน Loop เก็บค่า "งานอดิเรก" ตามรูป
-    let interest = "";
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value;
-        // ถ้าไม่ใช่ตัวสุดท้าย ให้เติมเครื่องหมาย comma (,) คั่น
-        if (i < interestDOMs.length - 1) {
-            interest += ", ";
+    let messageDOM = document.getElementById('message');
+    
+    try {
+    let interest = ''
+    for (let i=0; i < interestDOMs.length; i++){
+        interest += interestDOMs[i].value 
+        if (i !== interestDOMs.length -1){
+            interest += ', '
         }
     }
 
-    // รวบรวมข้อมูลลงใน Object userData
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
+    let userData ={
+        firstname: firstNameDOM.value,
+        lastname: lastNameDOM.value,
         age: ageDOM.value,
-        gender: genderDOM ? genderDOM.value : "", // กัน error ถ้าไม่ได้เลือกเพศ
+        gender: genderDOM.value,
         description: descriptionDOM.value,
-        interest: interest
-    };
-
-    // แสดงผลออกทาง Console
-    console.log('submitData', userData);
-}
+        interests: interest
+    }
+    
+            const response = await axios.post("http://localhost:8000/users", userData)
+            console.log('response', response);
+            messageDOM.innerText = "บันทึกข้อมูลสำเร็จ!";
+            messageDOM.className = "message success";
+    } catch (error) {
+        if (error.response) {
+            console.log('Error response:', error.response.data);
+        }
+            messageDOM.innerText = "บันทึกข้อมูลไม่สำเร็จ!";
+            messageDOM.className = "message error";
+    }
+    console.log('submit data: ', userData);
+    }
